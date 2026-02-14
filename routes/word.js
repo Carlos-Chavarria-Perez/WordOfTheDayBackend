42cea575f48a0f25a9b4d82cfffc6ed787172511
Carlos Chavarria <chato12_7@hotmail.com>
2026-02-14 14:20:26 -0600

    Fix import casing for WordofDayController (Linux case-sensitive filesystems)

--- routes/word.js
+++ routes/word.js
@@ -1,6 +1,6 @@
 // routes/word.routes.js
 import express from "express";
-import { getRandomWordData } from "../controllers/WordofDayController.js";
+import { getRandomWordData } from "../Controllers/WordofDayController.js";
 
 const router = express.Router();
 
